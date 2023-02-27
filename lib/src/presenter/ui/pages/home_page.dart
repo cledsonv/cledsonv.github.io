b2d9c117f4a0517685cd2f359f12ef3fe772d647
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-02-27 02:28:16 -0300

    visual changing

--- lib/src/presenter/ui/pages/home_page.dart
+++ lib/src/presenter/ui/pages/home_page.dart
@@ -11,53 +11,56 @@ class HomePage extends StatelessWidget {
     SocialController sc = SocialController();
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 20),
-              const Center(
-                child: CircleAvatar(
-                  radius: 140,
-                  backgroundImage: NetworkImage(
-                      'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650'),
-                ),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                                text: 'Hello welcome to my portifolio\n',
+                                style: Theme.of(context).textTheme.titleLarge),
+                            TextSpan(
+                                text: "I'm Cledson,\n",
+                                style:
+                                    Theme.of(context).textTheme.displayMedium),
+                            TextSpan(
+                                text: 'A Mobile Aplication Developper\n',
+                                style:
+                                    Theme.of(context).textTheme.displaySmall),
+                            TextSpan(
+                                text:
+                                    "I'm starting my career now as a Mobile Progamator",
+                                style: Theme.of(context).textTheme.titleLarge),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  const CircleAvatar(
+                    radius: 170,
+                    backgroundImage: NetworkImage(
+                        'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650'),
+                  ),
+                ],
               ),
-              const SizedBox(height: 10),
-              Center(
-                child: Text(
-                  'Cledson Ventura',
-                  style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                      fontWeight: FontWeight.bold, color: Colors.black),
-                ),
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text('Technologies',
+                      style: Theme.of(context).textTheme.displaySmall),
+                ],
               ),
-              const SizedBox(height: 20),
-              Text(
-                'Redes Sociais',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: sc.socialModel.length,
-                  itemBuilder: (context, idx) {
-                    final SocialMediaEntity social = sc.socialModel[idx];
-
-                    return SocialCard(
-                        link: social.link,
-                        image: social.image,
-                        description: social.name);
-                  },
-                ),
-              )
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
